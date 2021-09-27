23f6b17ab591a82d3ccb330d22ed74431ff48dc6
navid khodaveisi <navidliverpool@yahoo.com>
2021-09-27 20:02:41 +0100

    commit one

---
+++ NavEcommerce/Models/MotorcyclesFolder/Brand.cs
@@ -0,0 +1,9 @@
+﻿
+namespace NavEcommerce.Models.MotorcyclesFolder
+{
+    public class Brand
+    {
+        public int IdBrand { get; set; }
+        public string NameBrand { get; set; }
+    }
+}
---
+++ NavEcommerce/Models/dbContextInstanses/NavCommerceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NavEcommerce.Models.MotorcyclesFolder;
+
+namespace NavEcommerce.Models.dbContextInstanses
+{
+    public class NavCommerceDbContext : DbContext
+    {
+        public DbSet<Motorcycle> Motorcycles { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
